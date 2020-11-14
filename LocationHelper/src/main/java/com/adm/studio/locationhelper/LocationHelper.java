ddb3d94ce1f2bb4a837971bf3a8ef2226906a500
MD. RONi <roni5point@gmail.com>
2020-11-14 13:49:49 +0600

    Initial commit

---
+++ LocationHelper/src/main/java/com/adm/studio/locationhelper/LocationHelper.java
@@ -0,0 +1,16 @@
+package com.adm.studio.locationhelper;
+
+import android.location.Location;
+
+public class LocationHelper {
+
+    public static float getDistanceKM(double startLatitude, double startLongitude, double endLatitude
+    , double endLongitude) {
+
+        float[] result = new float[0];
+        Location.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude, result);
+
+        return result[0] / 1000;
+    }
+
+}
