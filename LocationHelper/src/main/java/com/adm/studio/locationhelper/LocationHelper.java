9594f65653c26a1014402a1ad4fac77ca1c485b6
MD. RONi <roni5point@gmail.com>
2020-11-14 17:07:34 +0600

    initial commit

--- LocationHelper/src/main/java/com/adm/studio/locationhelper/LocationHelper.java
+++ LocationHelper/src/main/java/com/adm/studio/locationhelper/LocationHelper.java
@@ -7,8 +7,6 @@ public class LocationHelper {
     public static float getDistanceKM(double startLatitude, double startLongitude, double endLatitude
     , double endLongitude) {
 
-        float[] result = new float[0];
-        Location.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude, result);
 
         return 1000;
     }
